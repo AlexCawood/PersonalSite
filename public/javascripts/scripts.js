e804c948f665d7e8ccca53c803de8b50fed684f2
Alexander Cawood <alexander.cawood@gmail.com>
2022-06-13 15:02:44 +0200

    Added a number of changes

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -21,7 +21,7 @@ function addProjects(imgUrl, title,projectUrl){
                             </div>
                         </a>
                     </div>
-                    <div class="col-12 pt-1 d-flex justify-content-center">
+                    <div class="col-12 ps-0 pt-2 d-flex justify-content-center">
                         <h5>${title}</h5>
                     </div>
                 </div>
@@ -32,10 +32,9 @@ function addProjects(imgUrl, title,projectUrl){
             console.log("Page does not contain element with correct Id");
         }
         
-
 }
 
-function blobAnimiation(stroke_width = 0,direction = false){
+function blobAnimiation(stroke_width = 0,direction = false, imageurl='images/Ragga_remove_background.png', x=0,y=0){
 
             // let element = ''
     
@@ -52,13 +51,8 @@ function blobAnimiation(stroke_width = 0,direction = false){
         }else if (direction === true){
             stroke_width = stroke_width - 0.10
         }
-        
-    
-    
-        
-    
+
         let element = `
-    
                         <defs> 
                             <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
                                 <stop id="stop1" stop-color="rgba(64, 113, 244, 1)" offset="0%"></stop>
@@ -66,7 +60,8 @@ function blobAnimiation(stroke_width = 0,direction = false){
                             </linearGradient>
                         </defs>
                         <path fill="url(#sw-gradient)" d="M20.1,-27.1C25.4,-23.8,28.5,-17.1,30.6,-10.1C32.7,-3.1,33.8,4.1,32.5,11.4C31.3,18.7,27.8,26,21.9,29.3C16.1,32.6,8.1,32,-0.8,33.1C-9.8,34.3,-19.5,37.3,-26.5,34.4C-33.5,31.4,-37.8,22.5,-38.7,14C-39.6,5.4,-37,-2.9,-34.8,-11.7C-32.5,-20.5,-30.4,-29.7,-24.6,-32.8C-18.9,-35.8,-9.4,-32.7,-1,-31.3C7.4,-29.9,14.8,-30.3,20.1,-27.1Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="${stroke_width}" style="transition: all 0.3s ease 0s;" stroke="url(#sw-gradient)"></path>
-    `
+                        <image href="${imageurl}" height="100" width="100" x="${x}" y="${y}" />
+                        `
         // console.log(element);
         try {
         document.getElementById('sw-js-blob-svg').innerHTML = element
@@ -76,23 +71,18 @@ function blobAnimiation(stroke_width = 0,direction = false){
         }
     
     }, 50) 
-    
-    
 
-
-    
 }
 
 addProjects('images/123cvLogo.svg','A CV Creating Telegram Bot', "https://123cv.co.za")
-addProjects('images/stock-unplash.jpeg','JSE Share Alert App', "https://www.jsealerts.co.za/")
-addProjects('images/Chair-Contact-pic.jpeg','Boutique Ecommerce Site with Wix', "https://www.thesilkpurse.co.za/")
+addProjects('images/stock-unplash-300-200.jpeg','JSE Share Alert App', "https://www.jsealerts.co.za/")
+addProjects('images/Chair-Contact-pic-134-200.jpeg','Boutique Ecommerce Site with Wix', "https://www.thesilkpurse.co.za/")
 
 // for (let i = 1; i <= 3; i++) {
 //     addProjects('images/123cvLogo.svg','Test title '+i, "https://123cv.co.za")
-    
 // }
 
-blobAnimiation(0,false)
+
 
 
 
--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,19 @@
 var express = require('express');
 const nodemailer = require('nodemailer');
 var router = express.Router();
+require('dotenv').config()
+
+var transporter = nodemailer.createTransport({
+  host:"smtpout.secureserver.net",
+  secure: false,
+  port:587,
+  secureConnection: false,
+  auth: {
+    user: process.env.email,
+    pass: process.env.password
+  }
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +21,26 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
+    if (!req.body) res.sendStatus(404)
+
+    console.log(req.body.message);
+    username = req.body.name.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    email = req.body.email.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    message = req.body.message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var mailOptions = {
+      from: process.env.email,
+      to: process.env.email,
+      subject: username+"has sent you a message",
+      text: message + '\n\nFrom: '+email
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
     // res.render('pages/contact', { title: 'contact' });
     res.render('pages/contact', { title: 'contact' });
   });
